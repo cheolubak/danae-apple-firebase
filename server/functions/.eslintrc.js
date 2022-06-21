d6f3c497cf2644a41e1e785568204f1204514a31
cheolubak <cheolu.bak@sebasi.co.kr>
2022-06-21 23:43:17 +0900

    init

---
+++ server/functions/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 'off',
+  },
+};
