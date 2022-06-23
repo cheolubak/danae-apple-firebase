bb5cc3f9c21f1d3a47498d24a7157b72956fcb32
B.Cheolu <cheolu.bak@sebasi.co.kr>
2022-06-23 23:17:49 +0900

    Init web with React

---
+++ appllications/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@danae/components';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button theme="primary">TEST</Button>
+    </>
+  );
+};
+
+export default App;
